934ee7d0de1e5b54dfc270fd2a96b27f403334a1
Diego Saravia <saravia__2000@hotmail.com>
2024-08-05 09:18:04 -0600

    Arreglando integración con materializecss

--- resources/views/templates/dashboard-side-menu.blade.php
+++ resources/views/templates/dashboard-side-menu.blade.php
@@ -38,7 +38,7 @@
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="{{ asset('css/materialize.min.css') }} " />
+    <link rel="stylesheet" href="{{ asset('css/materialize.min.css') }} " />
     @vite(['resources/scss/side-navbar.scss', 'resources/scss/resets.scss'])
     @yield('styles')
 </head>
