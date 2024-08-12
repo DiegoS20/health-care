8aa4565b6be11d86153b2b073812d1b1117c08bc
Diego Saravia <saravia__2000@hotmail.com>
2024-08-11 18:30:36 -0600

    Pantalla principal de medicinas

--- resources/views/dashboard/medicinas.blade.php
+++ resources/views/dashboard/medicinas.blade.php
@@ -2,6 +2,37 @@
 
 @section('title', 'Medicinas')
 
+@section('styles')
+    @vite(['resources/scss/search-add-bar.scss', 'resources/scss/table.scss'])
+    <style>
+        .table {
+            margin-top: 50px
+        }
+    </style>
+@endsection
+
+@php
+    $headers = ['Nombre', 'Presentación', 'Concentración', 'Cantidad disponible', 'Precio'];
+
+    $data = [
+        [
+            'Nombre' => 'Paracetamol',
+            'Presentación' => 'Tabletas',
+            'Concentración' => '500 mg',
+            'Cantidad disponible' => 100,
+            'Precio' => '$5.00',
+        ],
+        [
+            'Nombre' => 'Ibuprofeno',
+            'Presentación' => 'Cápsulas',
+            'Concentración' => '200 mg',
+            'Cantidad disponible' => 30,
+            'Precio' => '$8.00',
+        ],
+    ];
+@endphp
+
 @section('content')
-    Medicinas
+    <x-search-add-bar searchUrl="{{ route('medicinas') }}" modalId="modal1" />
+    <x-table :headers="$headers" :tableData="$data" />
 @endsection
